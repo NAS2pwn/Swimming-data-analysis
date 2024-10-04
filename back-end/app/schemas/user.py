78e975cf8e469224c360943d67c9f44ac6d144d6
NAS2pwn <nassim.bennouar.mail@gmail.com>
2024-10-04 08:22:42 +0200

    tests created

--- back-end/app/schemas/user.py
+++ back-end/app/schemas/user.py
@@ -12,4 +12,4 @@ class User(UserBase):
     api_key: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
--- back-end/tests/test_api.py
+++ back-end/tests/test_api.py
@@ -1,18 +1,32 @@
 from fastapi.testclient import TestClient
 from app.main import app
-from unittest.mock import patch
-from app.models.models import User
-from sqlalchemy.orm import Session
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+import unittest
+from unittest.mock import patch, MagicMock
+from app.schemas.user import UserCreate
+from app.crud.user import create_user
 
 client = TestClient(app)
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the Swimming Olympics API"}
+class TestEndpoints(unittest.TestCase):
+    def test_read_main(self):
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Welcome to the Swimming Olympics API"}
 
-#def test_endpoint_create_user():
+    @patch('app.crud.user.Session')
+    def test_create_user(self, mock_session):
+        user_data = UserCreate(email="test@test.fr", password="hehehe")
+        
+        mock_db = MagicMock()
+        mock_session.return_value = mock_db
+        
+        created_user = create_user(mock_db, user_data)
+        
+        mock_db.add.assert_called_once()
+        mock_db.commit.assert_called_once()
+        mock_db.refresh.assert_called_once()
+        
+        assert created_user.email == user_data.email
+        assert created_user.is_admin == False
+        assert hasattr(created_user, 'api_key')
+        assert created_user.hashed_password != user_data.password
